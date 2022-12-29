f2d3856a927223fd4363903547ad17ec55425b66
keschaefer <keschaefer@gmail.com>
2022-12-29 16:44:50 -0700

    add experience content

--- src/Sections/EducationSection.jsx
+++ src/Sections/EducationSection.jsx
@@ -4,14 +4,12 @@ import { HEADER_HEIGHT_REMS } from "../Header";
 
 const EducationSection = () => {
   return (
-    <SectionWrapper>
-      <div
-        className={`scroll-mt-${HEADER_HEIGHT_REMS} snap-start h-section bg-slate-800`}
-        id={VIEW_SECTIONS.EDUCATION}
-      >
-        <Title title="Education" />
-      </div>
-    </SectionWrapper>
+    <div
+      className={`scroll-mt-${HEADER_HEIGHT_REMS} snap-start h-section bg-slate-800`}
+      id={VIEW_SECTIONS.EDUCATION}
+    >
+      <Title title="Education" />
+    </div>
   );
 };
 
